b2efc5021d5ed57083055ef52d43b9e1376c9bcb
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-09 12:40:08 +0530

    nav added

---
+++ src/components/logo/Logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import logo from '../../assests/img/logo.svg';
+
+const Logo = () => {
+  return (
+    <div>
+      <img src={logo} alt='logo is here' className='img' />
+    </div>
+  );
+};
+
+export default Logo;
