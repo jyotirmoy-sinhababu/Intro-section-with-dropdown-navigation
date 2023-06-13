8a6b083c37c544140b24c47f6871c9b7dc6555ce
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-13 15:40:52 +0530

    navStyle created

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -3,16 +3,28 @@ import React from 'react';
 import Logo from '../logo/Logo';
 import FeatureBtn from '../sub-comp/navBtns/Feature/FeatureBtn';
 import CompanyBtn from '../sub-comp/navBtns/company/CompanyBtn';
+import Auth from '../sub-comp/navBtns/auth/Auth';
 
 const Nav = () => {
   return (
-    <div>
-      <div>
-        <Logo />
+    <div className='nav-cnt'>
+      <div className='nav-logo-btn-cnt'>
+        <div className='nav-logo-cnt'>
+          <Logo />
+        </div>
+        <div className='nav-btn-cnt'>
+          <FeatureBtn />
+          <CompanyBtn />
+          <div>
+            <button>Careers</button>
+          </div>
+          <div>
+            <button>About</button>
+          </div>
+        </div>
       </div>
-      <div>
-        <FeatureBtn />
-        <CompanyBtn />
+      <div className='nav-auth-cnt'>
+        <Auth />
       </div>
     </div>
   );
---
+++ src/components/sub-comp/navBtns/auth/Auth.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Auth = () => {
+  return (
+    <div>
+      <div>
+        <button>Login</button>
+      </div>
+      <div>
+        <button>Register</button>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
