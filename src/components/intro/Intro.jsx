8f35669cff897188fb90e9a976cd0a16d7e288ab
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-14 01:26:10 +0530

    intro comp created

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import audio from '../../assests/img/client-audiophile.svg';
+import databiz from '../../assests/img/client-databiz.svg';
+import maker from '../../assests/img/client-maker.svg';
+import deskImg from '../../assests/img/image-hero-desktop.png';
+import mobImg from '../../assests/img/image-hero-mobile.png';
+
+const Intro = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Make remote work</h1>
+          <p>
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </p>
+        </div>
+        <div>
+          <button>Learn More</button>
+        </div>
+        <div>
+          <img src={databiz} alt='' />
+          <img src={audio} alt='' />
+          <img src={maker} alt='' />
+        </div>
+      </div>
+      <div>
+        <img src={deskImg} alt='' />
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Nav from '../../components/nav/Nav';
+import Intro from '../../components/intro/Intro';
 
 const Home = () => {
   return (
     <div>
       <Nav />
+      <Intro />
     </div>
   );
 };
