acdf4075e6b4a0af17481a72ad5e25c64f89423a
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-14 00:03:17 +0530

    style added

--- src/components/sub-comp/navBtns/navMobileView/NavMobileBtn.jsx
+++ src/components/sub-comp/navBtns/navMobileView/NavMobileBtn.jsx
@@ -1,17 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import FeatureBtn from '../Feature/FeatureBtn';
+import CompanyBtn from '../company/CompanyBtn';
 
 import './navMobileStyle.css';
+import Auth from '../auth/Auth';
 
 const NavMobileBtn = () => {
+  const [isNavBtn, SetIsNavBtn] = useState(false);
+
+  const controlFeature = () => {
+    if (!isNavBtn) {
+      SetIsNavBtn(true);
+    }
+    if (isNavBtn) {
+      SetIsNavBtn(false);
+    }
+  };
   return (
     <div className='nav-mobile-btn-cnt'>
-      <button className='nav-mobile-btn'>
-        <div className='nav-1stspan'></div>
-        <div className='nav-2ndspan'></div>
-        <div className='nav-3rdspan'></div>
+      <button
+        onClick={() => {
+          controlFeature();
+        }}
+        className='nav-mobile-btn'
+      >
+        <div className={`nav-1stspan ${isNavBtn ? 'one' : 'null'}`}></div>
+        <div className={`nav-2ndspan ${isNavBtn ? 'two' : 'null'}`}></div>
+        <div className={`nav-3rdspan ${isNavBtn ? 'three' : 'null'}`}></div>
       </button>
       <div>
-        <div></div>
+        {isNavBtn ? (
+          <div>
+            <div>
+              {' '}
+              <FeatureBtn />
+              <CompanyBtn />
+              <button>Careers</button>
+              <button>About</button>
+            </div>
+            <div>
+              <Auth />
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
