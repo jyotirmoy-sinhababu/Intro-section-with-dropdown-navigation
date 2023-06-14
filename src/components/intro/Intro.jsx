d99bbcad849b1548ded44d6a2ef6c50bf64ef595
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-06-14 13:12:36 +0530

    style added

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -11,25 +11,26 @@ import mobImg from '../../assests/img/image-hero-mobile.png';
 const Intro = () => {
   return (
     <div className='intro-cnt'>
-      <div>
-        <div>
-          <h1>Make remote work</h1>
-          <p>
+      <div className='intro-txt-cnt'>
+        <div className='intro-txt-first'>
+          <h1 className='intro-header'>Make remote work</h1>
+          <p className='intro-para'>
             Get your team in sync, no matter your location. Streamline
             processes, create team rituals, and watch productivity soar.
           </p>
         </div>
         <div>
-          <button>Learn More</button>
+          <button className='intro-btn'>Learn More</button>
         </div>
-        <div>
+        <div className='intro-imgs'>
           <img src={databiz} alt='' />
           <img src={audio} alt='' />
           <img src={maker} alt='' />
         </div>
       </div>
       <div>
-        <img className='main-img' src={deskImg} alt='' />
+        <img className='main-img' src={deskImg} alt='desktop img' />
+        <img className='mobile-img' src={mobImg} alt='mobile img' />
       </div>
     </div>
   );
